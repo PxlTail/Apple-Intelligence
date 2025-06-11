d39b169e6de007d3d4927e78d1ca4b123b243cfe
tahoe <tahoe@tahoes-Mac-Studio.local>
2025-06-10 23:03:14 -0700

    Major bug fix attempt 4?

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { spawn } from "child_process"
 import express, { response } from "express"
 import bodyParser from "body-parser";
+import { fileURLToPath } from 'url';
 import path from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
